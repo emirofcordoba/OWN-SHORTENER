0771fd382deb173784acbd52bfdcbb2b9a784d53
Calinion (Nerd) <159137813+emirofcordoba@users.noreply.github.com>
2024-05-21 20:18:45 +0600

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+![MogaShortener Logo](https://github.com/emirofcordoba/OWN-SHORTENER/blob/main/logo.png)
+
+# **This project is made by [t.me/emirofcordoba](https://t.me/MogaTeam) alone**
+
+# **MogaShortener**
+
+## **This project is in Node.js, and its purpose is to help you create your own link shortener Telegram bot hosted on your custom domain.**
+
+## **Installation Instructions**
+
+## **Steps to set up the project at Glitch:**
+
+<p align="center">
+  <a href="https://glitch.com/edit/#!/import/git?url=https://github.com/emirofcordoba/OWN-SHORTENER.git">
+    <img src="https://img.shields.io/badge/Import%20To%20Glitch-Click%20to%20Import-brightgreen?style=for-the-badge" alt="Import to Glitch Button" width="820" />
+  </a>
+</p>
+
+1. **Step one** - Click the "**Import To Glitch**" button
+2. **Step two** - Wait some time for the project to be synced
+3. **Step three** - Go to the `.env` file in the project and input the proper variables
+4. **Step four** - Click on "**🔎 Preview**" in the top right corner at the bottom
+5. **Step five** - Click on the second option under preview 
+6. **Step six** - Done, now enjoy your own shortener on your own domain 😍
+7. **Step seven** - If you want to change your shortener domain then change your Glitch project name 📛 
+## **Video Tutorial** ##
+## **[![Video Tutorial](https://github.com/emirofcordoba/OWN-SHORTENER/blob/main/vid.png)](https://github.com/emirofcordoba/OWN-SHORTENER/blob/main/vid.mp4?raw=true)**
+
+## **Social Contacts**
+
+<p align="center">
+  <a href="https://t.me/emirofcordoba">
+    <img src="https://img.shields.io/badge/CONTACT-TELEGRAM-blue?style=for-the-badge&logo=telegram" alt="Telegram Badge"/>
+  </a>
+  <a href="https://instagram.com/emirofcordoba">
+    <img src="https://img.shields.io/badge/CONTACT-INSTAGRAM-red?style=for-the-badge&logo=instagram" alt="Instagram Badge"/>
+  </a>
+  <a href="https://www.youtube.com/@highjacker_kidnapper/about">
+    <img src="https://img.shields.io/badge/CONTACT-YOUTUBE-red?style=for-the-badge&logo=youtube" alt="Youtube Badge"/>
+  </a>
+</p>
---
+++ server.js
@@ -0,0 +1,279 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var fs = require('fs').promises;
+var path = require('path');
+var axios = require('axios');
+var app = express();
+var PORT = 3000;
+var filePath = 'first.json';
+var URLS_FILE = path.join(__dirname, 'urls.json');
+var fileContent = fs.readFileSync(filePath, 'utf8');
+var jsonData = JSON.parse(fileContent);
+var ownerChatId = jsonData[Object.keys(jsonData)[0]];
+async function readDomainDataFromFile() {
+  var domainData = await fs.readFile('domain.txt', 'utf8');
+  return domainData;
+}
+app.use(bodyParser.json());
+var TelegramBot = require('node-telegram-bot-api');
+var CHANNEL_USERNAME = process.env.CHANNEL_USERNAME;
+var bot = new TelegramBot(process.env.TOKEN, { polling: true });
+var joinChannelButton = {
+text: 'JOIN CHANNEL👻',
+url: process.env.JOIN_CHANNEL_URL,
+};
+var joinedButton = {
+text: 'JOINED🥁',
+callback_data: 'check_joined'
+};
+
+async function sendJoinChannelMessage(chatId) {
+    var options = {
+        parse_mode: 'HTML',
+        reply_markup: {
+            inline_keyboard: [
+                [joinChannelButton, joinedButton]
+            ]
+        }
+    };
+    var message = "You can use this Telegram bot to shorten any link 🤩, but you're not joined to our channel. Please join and click 'Joined🙂'";
+    await bot.sendMessage(chatId, `<pre>${message}</pre>`, options);
+}
+
+bot.on('message', async (msg) => {
+  var chatId = msg.chat.id;
+
+  try {
+    // Read the existing file or initialize with an empty array if file doesn't exist
+    let chatIds;
+    try {
+      var data = await fs.readFile(filePath, 'utf-8');
+      chatIds = JSON.parse(data);
+    } catch (error) {
+      if (error.code === 'ENOENT') {
+        chatIds = [];
+      } else {
+        throw error;
+      }
+    }
+
+    // If no chat ID is saved yet, save the current chat ID
+    if (chatIds.length === 0) {
+      chatIds.push(chatId);
+      await fs.writeFile(filePath, JSON.stringify(chatIds, null, 2));
+      var message = '<pre>Your chat ID has been recorded as the owner.</pre>';
+      await bot.sendMessage(chatId, message, { parse_mode: 'HTML' });
+    }
+    // No response for other users if the owner chat ID is already set
+  } catch (error) {
+    console.error('Error handling message:', error.message);
+    var errorMessage = '<pre>An error occurred while processing your request.</pre>';
+    await bot.sendMessage(chatId, errorMessage, { parse_mode: 'HTML' });
+  }
+});
+
+bot.onText(/\/start/, async (msg) => {
+    var chatId = msg.chat.id;
+    var member = await bot.getChatMember(CHANNEL_USERNAME, chatId);
+
+    if (member.status === 'member' || member.status === 'administrator' || member.status === 'creator') {
+        await startCommand(chatId);
+    } else {
+        await sendJoinChannelMessage(chatId);
+    }
+});
+
+bot.on('callback_query', async (query) => {
+    var chatId = query.message.chat.id;
+
+    if (query.data === 'check_joined') {
+        var member = await bot.getChatMember(CHANNEL_USERNAME, chatId);
+
+        if (member.status === 'member' || member.status === 'administrator' || member.status === 'creator') {
+            await startCommand(chatId);
+        } else {
+            var randomMessage = "You didn't join all our communities, please join them first to use me perfectly 🥹";
+
+            await bot.answerCallbackQuery(query.id, {
+                text: randomMessage,
+                show_alert: true
+            });
+
+            await sendJoinChannelMessage(chatId);
+        }
+    }
+});
+
+bot.on('callback_query', async (query) => {
+    var chatId = query.message.chat.id;
+    var userId = query.from.id;
+    var data = query.data;
+
+    if (data === 'shorten_url') {
+        var member = await bot.getChatMember(CHANNEL_USERNAME, userId);
+
+        if (member.status === 'member' || member.status === 'administrator' || member.status === 'creator') {
+            await bot.sendMessage(chatId, "Send the URL you wanna shorten🔗", { reply_markup: { force_reply: true } });
+        } else {
+            await sendJoinChannelMessage(chatId);
+        }
+    }
+});
+
+async function startCommand(chatId) {
+    var message = "You can use this shorter bot to shorten any URL easily, just use the button below and shorten your links 🥁🤩😍";
+    var options = {
+        parse_mode: 'HTML',
+        reply_markup: {
+            inline_keyboard: [
+                [{ text: "Shorten🔗", callback_data: "shorten_url" }]
+            ]
+        }
+    };
+    await bot.sendMessage(chatId, `<pre>${message}</pre>`, options);
+}
+
+bot.on('message', async (msg) => {
+    if (msg.reply_to_message && msg.reply_to_message.text === "Send the URL you wanna shorten🔗") {
+        var chatId = msg.chat.id;
+        var userId = msg.from.id;
+        var url = msg.text;
+
+        try {
+            var member = await bot.getChatMember(CHANNEL_USERNAME, userId);
+            if (member.status === 'member' || member.status === 'administrator' || member.status === 'creator') {
+                
+                var currentUrl = await readDomainDataFromFile();
+                var response = await axios.post(currentUrl, { url });
+                var shortenedUrl = `<b>${response.data.short_url}</b>`;
+                var message = `<pre>Your URL was shortened</pre>\n\n${shortenedUrl}`;
+                var options = {
+                    parse_mode: 'HTML'
+                };
+                await bot.sendMessage(chatId, message, options);
+            } else {
+                
+                await sendJoinChannelMessage(chatId);
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            var errorMessage = "An error occurred while processing your request. Please try again later.";
+            await bot.sendMessage(chatId, errorMessage, { parse_mode: 'HTML' });
+        }
+    }
+});
+
+var loadUrls = async () => {
+    try {
+        var data = await fs.readFile(URLS_FILE, 'utf-8');
+        return JSON.parse(data);
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            return {};
+        } else {
+            throw err;
+        }
+    }
+};
+
+var saveUrls = async (urls) => {
+    await fs.writeFile(URLS_FILE, JSON.stringify(urls, null, 2));
+};
+
+var generateCombo = () => {
+    var length = Math.floor(Math.random() * 5) + 4;
+    var characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let combo = '';
+    for (let i = 0; i < length; i++) {
+        combo += characters.charAt(Math.floor(Math.random() * characters.length));
+    }
+    return combo;
+};
+
+app.post('/fetched', async (req, res) => {
+  var { message } = req.body;
+
+  const text = `<pre>Your project was fetched automatically by @emirofcordoba's system to provide you uptime experience🤩</pre>`;
+
+  try {
+    await bot.sendMessage(ownerChatId, text, { parse_mode: 'HTML' });
+    res.status(200).send('Notification received');
+  } catch (error) {
+    console.error('Error sending message:', error);
+    res.status(500).send('Failed to send notification');
+  }
+});
+
+var normalizeUrl = (url) => {
+    return url.replace(/https?:\/\/|www\./g, '').replace(/\/$/, '');
+};
+
+app.post('/', async (req, res) => {
+    var { url } = req.body;
+    var currentUrl = await readDomainDataFromFile();
+    if (!url) {
+        return res.status(400).json({ error: 'Missing "url" in request body' });
+    }
+
+    try {
+        var urls = await loadUrls();
+
+        var normalizedUrl = normalizeUrl(url);
+
+        let existingCombo = Object.keys(urls).find(key => normalizeUrl(urls[key]) === normalizedUrl);
+        if (existingCombo) {
+            var shortUrl = `${currentUrl}/${existingCombo}`;
+            return res.json({ short_url: shortUrl });
+        }
+
+        let combo;
+        do {
+            combo = generateCombo();
+        } while (urls[combo]);
+
+        urls[combo] = url;
+        await saveUrls(urls);
+
+        var shortUrl = `${currentUrl}/${combo}`;
+        res.json({ short_url: shortUrl });
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.get('/:combo', async (req, res) => {
+    var combo = req.params.combo;
+
+    try {
+        var urls = await loadUrls();
+
+        if (urls[combo]) {
+            var originalUrl = urls[combo];
+            res.redirect(originalUrl.startsWith('http') ? originalUrl : `http://${originalUrl}`);
+        } else {
+            res.status(404).send('URL not found');
+        }
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.get('/', async (req, res) => {
+  try {
+    var hostURL = 'http://' + req.get('host');
+    await fs.writeFile('domain.txt', hostURL);
+    res.send("Bot is up");
+
+    // Check if environment variables are present
+    if (process.env.JOIN_CHANNEL_URL && process.env.CHANNEL_USERNAME && process.env.TOKEN) {
+      await axios.post('https://open-saver-open.glitch.me', { url: hostURL });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
